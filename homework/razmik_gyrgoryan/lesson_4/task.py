d2365d2080db709d4470a75a36f904bbacc991b9
RazmikGrygoryan <razmik97@list.ru>
2024-02-06 11:34:35 +0300

    modifed lesson 4

--- homework/razmik_gyrgoryan/lesson_4/task.py
+++ homework/razmik_gyrgoryan/lesson_4/task.py
@@ -5,7 +5,7 @@ my_dict = {"tuple": (1, 2, 3, 4, 5), "list": [1, 2, 3, 4, 5],
 print(my_dict["tuple"][-1])
 my_dict["list"].append(7)
 my_dict["list"].pop(1)
-my_dict["dict"]["i am a tuple"] = 7
+my_dict["dict"][('i am a tuple',)] = 'i am a tuple'
 my_dict["dict"].pop("one")
 my_dict["set"].add(7)
 my_dict["set"].remove(3)
