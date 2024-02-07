c869ceaf82f4af2605d98362b68459b4d9e688a9
RazmikGrygoryan <razmik97@list.ru>
2024-02-07 10:58:43 +0300

    fixed lesson 5

--- homework/razmik_gyrgoryan/lesson_5/task_2.py
+++ homework/razmik_gyrgoryan/lesson_5/task_2.py
@@ -2,6 +2,18 @@ first_result = "результат операции: 42"
 second_result = "результат операции: 514"
 third_result = "результат работы программы: 9"
 
-print(int(first_result[first_result.index("42"):]) + 10)
-print(int(second_result[second_result.index("514"):]) + 10)
-print(int(third_result[third_result.index("9"):]) + 10)
+first_index = first_result.index(":")
+get_first_number_from_string = first_result[first_index::]
+get_first_number_from_string = get_first_number_from_string.lstrip(": ")
+
+second_index = second_result.index(":")
+get_second_number_from_string = second_result[second_index::]
+get_second_number_from_string = get_second_number_from_string.lstrip(": ")
+
+third_index = third_result.index(":")
+get_third_number_from_string = third_result[third_index::]
+get_third_number_from_string = get_third_number_from_string.lstrip(": ")
+
+print(int(get_first_number_from_string) + 10)
+print(int(get_second_number_from_string) + 10)
+print(int(get_third_number_from_string) + 10)
