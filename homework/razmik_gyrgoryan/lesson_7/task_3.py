8f7494ab5570f1261bbd39349403af456e06e53a
RazmikGrygoryan <razmik97@list.ru>
2024-02-08 15:06:37 +0300

    Refactor task 3

--- homework/razmik_gyrgoryan/lesson_7/task_3.py
+++ homework/razmik_gyrgoryan/lesson_7/task_3.py
@@ -1,10 +1,15 @@
 def some_program(first_result, second_result, third_result, last_result):
-    return (int(first_result.split()[-1]) + 10,
-            int(second_result.split()[-1]) + 10,
-            int(third_result.split()[-1]) + 10,
-            int(last_result.split()[-1]) + 10)
+    return (
+        int(first_result.split()[-1]) + 10,
+        int(second_result.split()[-1]) + 10,
+        int(third_result.split()[-1]) + 10,
+        int(last_result.split()[-1]) + 10
+    )
 
 
 print(*some_program(
-    "результат операции: 42", "результат операции: 54", "результат работы программы: 209", "результат: 2")
-      )
+    "результат операции: 42",
+    "результат операции: 54",
+    "результат работы программы: 209",
+    "результат: 2"
+))
