5416843dd64a9433578c67bf5013fce2880d5f45
RazmikGrygoryan <razmik97@list.ru>
2024-02-10 19:52:00 +0300

    refactor lesson 8

--- homework/razmik_gyrgoryan/lesson_8/task_2.py
+++ homework/razmik_gyrgoryan/lesson_8/task_2.py
@@ -1,12 +1,13 @@
 import gmpy2
 
 
-def fib_rec(length):
+def fib_rec():
     a, b = gmpy2.mpz(1), gmpy2.mpz(1)
-    for i in range(length):
+    while True:
         yield a
         a, b = b, a + b
 
 
-list_fibonaci = list(fib_rec(100000))
-print(list_fibonaci[4], list_fibonaci[99], list_fibonaci[999], list_fibonaci[99999])
+for index, i in enumerate(fib_rec()):
+    if index in [4, 99, 999, 99999]:
+        print(i)
