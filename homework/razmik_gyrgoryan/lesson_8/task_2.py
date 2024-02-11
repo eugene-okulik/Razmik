ea044ef858c762f21369c9979b5c36ccc763a42f
RazmikGrygoryan <razmik97@list.ru>
2024-02-11 18:55:12 +0300

    Try again lesson 8

--- homework/razmik_gyrgoryan/lesson_8/task_2.py
+++ homework/razmik_gyrgoryan/lesson_8/task_2.py
@@ -8,6 +8,10 @@ def fib_rec():
         a, b = b, a + b
 
 
+desired_numbers = [4, 99, 999, 99999]
+
 for index, i in enumerate(fib_rec()):
-    if index in [4, 99, 999, 99999]:
+    if index in desired_numbers:
         print(i)
+    if index == max(desired_numbers):
+        break
