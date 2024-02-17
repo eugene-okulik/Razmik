3479c2768bc7e5a9d7597e450757afe117e840ff
RazmikGrygoryan <razmik97@list.ru>
2024-02-17 07:36:27 +0300

    First try lesson 12

---
+++ homework/razmik_gyrgoryan/lesson_12/task_1.py
@@ -0,0 +1,56 @@
+class Flowers:
+    def __init__(self, svejest, color, len_steble, price, life_time):
+        self.__svejest = svejest
+        self.__color = color
+        self.__len_steble = len_steble
+        self.__price = price
+        self.__life_time = life_time
+
+    @property
+    def svejest(self):
+        return self.__svejest
+
+    @property
+    def color(self):
+        return self.__color
+
+    @property
+    def len_steble(self):
+        return self.__len_steble
+
+    @property
+    def price(self):
+        return self.__price
+
+    @property
+    def life_time(self):
+        return self.__life_time
+
+
+class Roses(Flowers):
+    def __init__(self):
+        super().__init__(svejest, color, len_steble, price, life_time)
+
+
+class Tulip(Flowers):
+    def __init__(self):
+        super().__init__(svejest, color, len_steble, price, life_time)
+
+
+class Bouquet:
+    def __init__(self):
+        self.flowers = flowers
+
+    def __add__(self, obj):
+        return self.flowers + obj
+
+    def avg_life_time(self):
+        total_life_time = sum(f.avg_life_time for f in self.flowers)
+        return total_life_time / len(self.flowers)
+
+    def find_flower(self, svejest, color, len_steble, price, life_time):
+        return self if svejest or color or len_steble or price or life_time else None
+
+
+rose = Roses()
+blue_flower = Tulip()
