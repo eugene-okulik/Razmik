6d31aa6d9dc2eee3cdb56859e7a8cee3269a6e4b
RazmikGrygoryan <razmik97@list.ru>
2024-02-19 08:57:27 +0300

    lesson 12 refactor methods

--- homework/razmik_gyrgoryan/lesson_12/task_1.py
+++ homework/razmik_gyrgoryan/lesson_12/task_1.py
@@ -1,10 +1,15 @@
 class Flowers:
-    def __init__(self, svejest, color, len_steble, price, life_time):
+    def __init__(self, title, svejest, color, len_steble, price, life_time):
+        self.__title = title
         self.__svejest = svejest
         self.__color = color
         self.__len_steble = len_steble
         self.__price = price
         self.__life_time = life_time
+
+    @property
+    def title(self):
+        return self.__title
 
     @property
     def svejest(self):
@@ -29,12 +34,12 @@ class Flowers:
 
 class Roses(Flowers):
     def __init__(self, svejest, color, len_steble, price, life_time):
-        super().__init__(svejest, color, len_steble, price, life_time)
+        super().__init__(svejest, "Rose", color, len_steble, price, life_time)
 
 
 class Tulip(Flowers):
     def __init__(self, svejest, color, len_steble, price, life_time):
-        super().__init__(svejest, color, len_steble, price, life_time)
+        super().__init__(svejest, "Tulip", color, len_steble, price, life_time)
 
 
 class Bouquet:
@@ -42,11 +47,29 @@ class Bouquet:
         self.flowers = flowers
 
     def __add__(self, obj):
-        return self.flowers + obj
+        return [self.flowers, obj]
 
     def avg_life_time(self):
         total_life_time = sum(f.avg_life_time for f in self.flowers)
         return total_life_time / len(self.flowers)
 
-    def find_flower(self):
-        return self if self.svejest or self.color or self.len_steble or self.price or self.life_time else None
+    def sort_svejest(self):
+        return sorted(self.flowers.svejest)
+
+    def sort_color(self):
+        return sorted(self.flowers.color)
+
+    def sort_len_steble(self):
+        return sorted(self.flowers.len_steble)
+
+    def sort_price(self):
+        return sorted(self.flowers.price)
+
+    def sort_life_time(self):
+        return sorted(self.flowers.life_time)
+
+    def find_flower(self, obj):
+        for flower in self.flowers:
+            if flower == obj:
+                return flower
+        return None
